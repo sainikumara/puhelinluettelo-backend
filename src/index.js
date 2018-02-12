9fb676923fc6c5148b63c11fd846e08a6dda6714
Kumara Saini M <saini@iki.fi>
2018-02-12 14:45:00 +0200

    Initiate app

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
+const cors = require('cors')
 
+app.use(cors())
 app.use(bodyParser.json())
 app.use(morgan('tiny'))
 
@@ -90,7 +92,7 @@ app.post('/api/persons', (req, res) => {
 })
   
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
